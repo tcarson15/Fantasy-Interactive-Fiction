51ac6c59b21d426263448610f7a7bc1e972a0f0d
Troy Carson <tcarson15@punahou.edu>
2014-02-12 13:40:24 -1000

    Wednesday Commit
    Testing

--- Character.java
+++ Character.java
@@ -5,24 +5,31 @@ public class Character{
   int strength;
   int ranged;
   int armorClass;
-  public Character(){
+  String name = new String; 
+  public Character(String n){
+    name = n;
     health = (int)math.random(20)+1;
     dexterity = (int)math.random(20)+1;
     strength = (int)math.random(20)+1;
     ranged = (int)math.random(20)+1;
     armorClass = dexterity;
   }
-  public Character(int h,int d, int s, int r, int ac){
-    health = r;
+  public Character(int h,int d, int s, int r, int ac, String n){
+    health = h;
     dextarity = d;
     strength = s;
     ranged = r;
     armorClass = ac;
+    name = n;
   }
-  public getHealth(){return health;}
-  public getDextarity(){return dexterity;}
-  public getStrength(){return strength;}
-  public getRanged(){return ranged;}
-  public getArmorClass(){return armorClass;}
+  public void getHealth(){return health;}
+  public void getDexterity(){return dexterity;}
+  public void getStrength(){return strength;}
+  public void getRanged(){return ranged;}
+  public void getArmorClass(){return armorClass;}
   
+  public void addHealth(int additionalHealth){health += additionalHealth;}
+  public void minusHealth(int subtractedHealth){health -= subtractedHealth;}
+  public void addDexterity(int additionalDexterity){dexterity += additionalDexterity;}
+  public void minusDexterity(intsubtractedDexterity){dexterity -= subtractedDexterity;}
 }
