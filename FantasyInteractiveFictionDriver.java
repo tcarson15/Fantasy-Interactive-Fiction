6e913ec07cc2e5a24839495cc769e021b65c29da
Troy Carson <tcarson15@punahou.edu>
2014-02-21 14:25:58 -1000

    2/21/14
    Worked more on combat, but minus a few compiling error, it should be
    fully completed now. But, the package is now acting up, I think it may
    be because I have symbols in the folder name, but fi i change it I
    can't save the files anymore and I can't commit to GitHub because the
    folder doesn't exist. I was playing around with it trying to fix it,
    but then I had to revert everything back to commit… super fun.

--- FantasyInteractiveFictionDriver.java
+++ FantasyInteractiveFictionDriver.java
@@ -6,9 +6,15 @@ public class FantasyInteractiveFictionDriver{
     public int event = 0;
     System.out.println("You there, Knight! What is your name?");
     String userName = keyboard.nextLine();
+    Character Player = new Character(userName);
+    player.addHealth(10);
+    player.addDexterity(10);
+    player.addStrength(10);
+    player.addRanged(10);
     System.out.println("Aye, I've heard of you, you've been trying to gain influence throughout the kingdom. I'm Captain Myzard of the Guard, and if you want to to make a real name for yourself, you can start by going over to Shatter Sands Tower to the East of here, trouble with bandits I hear, and we havn't the men to spare with the Civil War on our doorsteps, so we need our men in the cities to keep order and remind the people the King rules these lands.");
     System.out.println("");
     System.out.println("Enter '1' to agree to go to Shatter Sands Tower. Enter '2' to ask more about the rebellion. Enter '3' to attack Captain Myzard and declare yourself with the rebellion.");
+    Character CaptainMyzard = new CustomCharacter(20, 20, 30, 20, 25, "Captain Myzard"); 
     if (keyboard.nextLine() == 1){
       event = 1;
     }
@@ -21,36 +27,63 @@ public class FantasyInteractiveFictionDriver{
       System.out.println("Enter '1' to agree to go to Shatter Sands Tower.Enter '2' to attack Captain Myzard and declare yourself with the rebellion.");
     }
     if (keyboard.nextLine() == 1){
+      
       event = 1;
     }
     if (keyboard.nextLine() == 2){
       event = 2;
     }
-  }
+  } 
+  
   public static boolean combat(Character p1, Character p2){
     boolean hasWon = false;
-    boolean turn = (p1.getDextarity+(int)math.random(20)+1)>(p2.getDextarity+(int)math.random(20)+1); //determine which player attacks first
-    while(!hasWon){
-      if(turn){
-        int move = p1.selectMove;
-        if(move==1){ //melee
-          if((int)math.random(20)+1>=p2.getArmorClass) p2.minusHealth(p1.getStrength);
-        }
-        else if(move==2){ //ranged
-          if((int)math.random(20)+1>=p2.getArmorClass) p2.minusHealth(p1.getRanged);
-        }
-        else if(Move==3){ //skip
-          turn = false;
-        }
-        
-        if(!turn){
+    boolean stillAlive = true;
+    boolean enemyStillAlive = true;
+    if (stillAlive == true && enemyStillAlive == true){
+      boolean turn = (p1.getDextarity+(int)math.random(20)+1)>(p2.getDextarity+(int)math.random(20)+1); //determine which player attacks first
+      while(!hasWon){
+        if(turn){
           int move = p1.selectMove;
           if(move==1){ //melee
-            if((int)math.random(20)+1>=p1.getArmorClass) p1.minusHealth(p2.getStrength);
+            if((int)math.random(20)+1>=p2.getArmorClass) p2.minusHealth(p1.getStrength);
+            if(p2.health <= 0){
+              enemyStillAlive = false;
+            }
           }
           else if(move==2){ //ranged
-            if((int)math.random(20)+1>=p1.getArmorClass) p1.minusHealth(p2.getRanged);
+            if((int)math.random(20)+1>=p2.getArmorClass) p2.minusHealth(p1.getRanged);
+            if(p2.health <= 0){
+              enemyStillAlive = false;
+            }
           }
+          else if(Move==3){ //skip
+            turn = false;
+          }
+          
+          if(!turn){
+            int move = p1.selectMove;
+            if(move==1){ //melee
+              if((int)math.random(20)+1>=p1.getArmorClass) p1.minusHealth(p2.getStrength);
+              if(p1.health <= 0){
+                stillAlive = false;
+              }
+            }
+            else if(move==2){ //ranged
+              if((int)math.random(20)+1>=p1.getArmorClass) p1.minusHealth(p2.getRanged);
+              if(p1.health <= 0){
+                stillAlive = false;
+              }
+              else if(Move==3){ //skip
+                turn = false;
+              }
+            }
+          }
+        }
+      }
+      if (stillAlive == false){
+        return false;
+        else if enemyStillAlive == false && stillAlive = true{
+          return true;
         }
       }
     }
